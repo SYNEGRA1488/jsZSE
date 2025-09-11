1b081100cc89e15e09b1dd85d4ed1d2314cd9201
4.5.0 <klevicev42@gmail.com>
2025-09-11 07:58:49 +0200

    lievichev_zewnetrzny.js

--- lievichev_zewnetrzny.js
+++ lievichev_zewnetrzny.js
@@ -1,2 +1,7 @@
 document.write("ostatnia modyfikacja strony" .fontcolor("#C0C0C0").fontsize(6)+"<br>"); 
+
 document.write(document.lastModified .strike());
+function okno_zamknij()  
+                       { 
+                             window.close() 
+                       } 
